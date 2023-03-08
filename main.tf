a0ffcecc127ccc6bd05361716c845cafbeae2852
“Raghda <raghda99@live.com>
2023-03-08 21:31:13 +0200

    second commit

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "modules"{
-    source="../modules" 
+    #source="../modules"
+    source="github.com/RaghdaSallam/2_Tier_Application_Terraform_Modules.git" 
     vpc_id=var.vpc_id
     subnets=var.subnets
     name=var.name
